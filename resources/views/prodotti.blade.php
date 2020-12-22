233e64260a0cd9b58833b7fd713a2b67ec23cc44
carmine <carminepepiciello@gmail.com>
2020-12-22 16:17:10 +0100

    pagina prodotto

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -2,6 +2,33 @@
 
 @section('content')
 
+    <section id="prodotto">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    @foreach($pasta as $value)
+                    <div class="main-box-prodotto">
+                        <div class="box-title">
+                            <h3>{{$value['titolo']}}</h3>
+                        </div>
+                        <div class="box-img">
+                            <div class="img-1">
+                                <img src="{{$value['src-h']}}" alt="">
+                            </div>
+                            <div class="img-2">
+                                <img src="{{$value['src-p']}}" alt="">
+                            </div>
+                        </div>
+                        <div class="box-text">
+                            <p>{{$value['descrizione']}}</p>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </section>
+
 @endsection
 
 
--- routes/web.php
+++ routes/web.php
@@ -144,5 +144,18 @@ Route::get('/', function () {
 
 //prodotti
 Route::get('/prodotti', function () {
-    return view('prodotti');
+    $data = [
+        'pasta' => [
+            [
+        "src" => "https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-m.jpg",
+        "src-h" => "https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-h.jpg",
+        "src-p" => "https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-p.jpg",
+        "titolo" => "N.4 Spaghetto Quadrato Bucato",
+        "tipo" => "lunga",
+        "cottura" => "6 min",
+        "peso" => "500g",
+        "descrizione" => "È la vera rivelazione della gamma! Lo Spaghetto Quadrato Bucato n.4 fa pensare subito ad una pasta molto succulenta che ha lo stesso potenziale dello Spaghetto Quadrato (N.1 Spaghetto Quadrato). La sua consistenza soda si sprigiona in bocca con un’esplosione di emozioni, grazie agli spessori corposi, al colore elegantemente ambrato, alla texture delicatamente ruvida, cangiante e piacevolissima al tatto che trattiene il condimento sulla superficie. <br> Da provare per rivoluzionare le sorti del primo piatto sia a casa che al ristorante.Dedicato a chi in cucina ama sperimentare nuove forme del gusto, ma vuole stupire affidandosi ad una pasta che garantisce ottime performance in cottura, lo Spaghetto Quadrato Bucato n .4 è il formato perfetto che racchiude tutte caratteristiche uniche di Pasta La Molisana. <br> Da provare per quelli che… il Bucatino già mi piace, lo Spaghetto Quadrato Bucato n .4 sarà il paradiso della pasta!"]
+            ]
+       ];
+    return view('prodotti', $data);
 })->name('prodotti');
